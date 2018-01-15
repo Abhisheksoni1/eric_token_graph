90a5b1807cb6c43b290080ed10cf6107e52349df
abhisheksoni <abhisheksoni500@gmail.com>
2018-01-16 00:40:34 +0530

    procfile added

--- tokenstats/wsgi.py
+++ tokenstats/wsgi.py
@@ -13,7 +13,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "investment_portfolio.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tokenstats.settings")
 
 application = get_wsgi_application()
 
