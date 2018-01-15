de3a78be682d6625301719c00c295b213e162cd8
abhisheksoni <abhisheksoni500@gmail.com>
2018-01-16 00:30:38 +0530

    procfile added

--- tokenstats/wsgi.py
+++ tokenstats/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tokenstats.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "investment_portfolio.settings")
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root='static')
+application.add_files('staticfiles', prefix='more-files/')
